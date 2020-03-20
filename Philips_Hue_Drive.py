8cecf1fec0a94f5d07812deb6dfb9b43a8e9b08f
walter5138_charon <piranha@disroot.org>
2020-03-20 14:41:00 +0100

    Restructured the code and added lamp_dict dialoge.

--- Philips_Hue_Drive.py
+++ Philips_Hue_Drive.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GdkPixbuf 
@@ -10,7 +10,6 @@ from hue_class import HueLamp
 for a, n in lamp_dict.items():
     globals()[n] = HueLamp(a, n)
 
-
 class MainWindow(Gtk.ApplicationWindow):
 
     def __init__(self, app):
@@ -18,7 +17,7 @@ class MainWindow(Gtk.ApplicationWindow):
         Gtk.Window.__init__(self, title="Box Test", application=app)
         #self.set_default_size(40, 40)
         self.set_border_width(10)
-        self.set_position(Gtk.WindowPosition.MOUSE)
+        self.set_position(Gtk.WindowPosition.CENTER)
         self.settings = Gtk.Settings.get_default()
         self.settings.set_property("gtk-application-prefer-dark-theme", True)
         
@@ -35,6 +34,7 @@ class MainWindow(Gtk.ApplicationWindow):
         hue_button.set_popup(main_menu)
         self.hb.pack_start(hue_button)
 
+
         admin_menu_item = Gtk.MenuItem(label="Hue Admin")
         main_menu.append(admin_menu_item)
 
@@ -43,8 +43,9 @@ class MainWindow(Gtk.ApplicationWindow):
 
         menuitem = Gtk.MenuItem(label="import new Bulb")
         admin_submenu.append(menuitem)
-        menuitem = Gtk.MenuItem(label="show lamp_dict")
-        admin_submenu.append(menuitem)
+        lamp_dict_mi = Gtk.MenuItem(label="show lamp_dict")
+        lamp_dict_mi.connect('activate', self.lamp_dict_mi_selected)
+        admin_submenu.append(lamp_dict_mi)
         quit_mi = Gtk.MenuItem(label="Quit")
         quit_mi.connect('activate', self.quit_menuitem_selected)
         admin_submenu.append(quit_mi)
@@ -71,11 +72,69 @@ class MainWindow(Gtk.ApplicationWindow):
 
         for lamp in lamp_dict.values():
             hl_obj = globals()[lamp]
-            self.grid = Gtk.Grid()
+            
+            dynamic_grid = Gtk.Grid()
+            
+            mir_label = Gtk.Label()
+            mir_label.set_text("mired")
 
-            label_lamp = Gtk.Label()
-            label_lamp.props.halign = Gtk.Align.START
-            label_lamp.set_text(lamp)
+            mir_adjust = Gtk.Adjustment(value=326, lower=153, upper=500, step_increment=20, page_increment=20, page_size=0)
+
+            mir_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=mir_adjust)
+            mir_scale.set_value_pos(Gtk.PositionType.RIGHT)
+            mir_scale.set_value(hl_obj.mired_get())
+            mir_scale.set_digits(0)
+            mir_scale.set_hexpand(True)
+            mir_scale.set_vexpand(False)
+            mir_scale.connect("value-changed", self.mir_scale_moved, hl_obj)
+
+
+            col_label = Gtk.Label()
+            col_label.set_text("color")
+
+            col_adjust = Gtk.Adjustment(value=326, lower=153, upper=500, step_increment=20, page_increment=20, page_size=0)
+
+            col_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=col_adjust)
+            col_scale.set_value_pos(Gtk.PositionType.RIGHT)
+            #col_scale.set_value(hl_obj.color_get())
+            col_scale.set_digits(0)
+            col_scale.set_hexpand(True)
+            col_scale.set_vexpand(False)
+            #col_scale.connect("value-changed", self.mir_scale_moved, hl_obj)
+
+
+            bri_label = Gtk.Label()
+            bri_label.set_text("brightness")
+
+            bri_adjust = Gtk.Adjustment(value=126, lower=1, upper=254, step_increment=10, page_increment=10, page_size=0)
+
+            bri_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=bri_adjust)
+            bri_scale.set_value_pos(Gtk.PositionType.RIGHT)
+            bri_scale.set_value(hl_obj.brightness_get())
+            bri_scale.set_digits(0)
+            bri_scale.set_hexpand(True)
+            bri_scale.set_vexpand(False)
+            bri_scale.connect("value-changed", self.bri_scale_moved, hl_obj)
+
+
+            tra_label = Gtk.Label()
+            tra_label.set_text("transitiontime")
+
+            tra_adjust = Gtk.Adjustment(value=1, lower=0, upper=4, step_increment=1, page_increment=1, page_size=0)
+
+            tra_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=tra_adjust)
+            tra_scale.set_value_pos(Gtk.PositionType.RIGHT)
+            tra_scale.set_value(hl_obj.transitiontime_get())
+            tra_scale.set_digits(0)
+            tra_scale.set_hexpand(True)
+            tra_scale.set_vexpand(False)
+            tra_scale.connect("value-changed", self.tra_scale_moved, hl_obj)
+
+
+
+            lamp_label = Gtk.Label()
+            lamp_label.props.halign = Gtk.Align.START
+            lamp_label.set_text(lamp)
 
             on_off_switch = Gtk.Switch()
             on_off_switch.props.valign = Gtk.Align.CENTER
@@ -93,11 +152,11 @@ class MainWindow(Gtk.ApplicationWindow):
             lamp_menu_button.add(image)
             lamp_menu_button.props.halign = Gtk.Align.START
 
-            main_menu = Gtk.Menu()
-            lamp_menu_button.set_popup(main_menu)
+            lamp_menu = Gtk.Menu()
+            lamp_menu_button.set_popup(lamp_menu)
 
             admin_menu_item = Gtk.MenuItem(label="Hue Admin")
-            main_menu.append(admin_menu_item)
+            lamp_menu.append(admin_menu_item)
 
             admin_submenu = Gtk.Menu()
             admin_menu_item.set_submenu(admin_submenu)
@@ -105,84 +164,50 @@ class MainWindow(Gtk.ApplicationWindow):
             menuitem = Gtk.MenuItem(label="set bulb name")
             admin_submenu.append(menuitem)
 
+            transitiontime_cmi = Gtk.CheckMenuItem(label="transitiontime")
+            transitiontime_cmi.connect("activate", self.transitiontime_cmi_selected, hl_obj, dynamic_grid, tra_label, tra_scale)
+            admin_submenu.append(transitiontime_cmi)
+
+
             fun_menu_item = Gtk.MenuItem(label="Hue Fun")
-            main_menu.append(fun_menu_item)
+            lamp_menu.append(fun_menu_item)
 
             fun_submenu = Gtk.Menu()
             fun_menu_item.set_submenu(fun_submenu)
-            
-            label_tra = Gtk.Label()
-            tra = Gtk.Adjustment(value=1, lower=0, upper=4, step_increment=1, page_increment=1, page_size=0)
-            self.tra_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=tra)
 
-            menuitem = Gtk.CheckMenuItem(label="mired")
-            fun_submenu.append(menuitem)
-            menuitem = Gtk.CheckMenuItem(label="color")
-            fun_submenu.append(menuitem)
-            menuitem = Gtk.CheckMenuItem(label="brightness")
-            fun_submenu.append(menuitem)
-            transitiontime_mi = Gtk.CheckMenuItem(label="transitiontime")
-            transitiontime_mi.connect("activate", self.transitiontime_mi_selected, hl_obj, self.grid, label_tra, self.tra_scale)
-            fun_submenu.append(transitiontime_mi)
+            mired_rmi= Gtk.RadioMenuItem(label="mired")
+            mired_rmi.connect("toggled", self.mired_rmi_selected, hl_obj, dynamic_grid, lamp_label, mir_label, mir_scale, col_label, col_scale)
+            mired_rmi.set_active(True)
+            fun_submenu.append(mired_rmi)
+            color_rmi= Gtk.RadioMenuItem(label="color", group=mired_rmi)
+            fun_submenu.append(color_rmi)
+            brightness_cmi = Gtk.CheckMenuItem(label="brightness")
+            brightness_cmi.connect("activate", self.brightness_cmi_selected, hl_obj, dynamic_grid, bri_label, bri_scale, on_off_switch, lamp_menu_button)
+            brightness_cmi.set_active(True)
+            fun_submenu.append(brightness_cmi)
 
-            main_menu.show_all()
+            lamp_menu.show_all()
 
-            label_mir = Gtk.Label()
-            label_mir.set_text("mired")
+            dynamic_grid.set_border_width(0)
+            dynamic_grid.set_column_homogeneous(True)
+            #dynamic_grid.set_row_homogeneous(False)
+            #dynamic_grid.set_column_spacing(40)
+            #dynamic_grid.set_row_spacing(40)
 
-            mir = Gtk.Adjustment(value=326, lower=153, upper=500, step_increment=20, page_increment=20, page_size=0)
-
-            mir_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=mir)
-            mir_scale.set_value_pos(Gtk.PositionType.RIGHT)
-            mir_scale.set_value(hl_obj.mired_get())
-            mir_scale.set_digits(0)
-            mir_scale.set_hexpand(True)
-            mir_scale.set_vexpand(False)
-            mir_scale.connect("value-changed", self.mir_scale_moved, hl_obj)
-
-
-            label_bri = Gtk.Label()
-            label_bri.set_text("brightness")
-
-            bri = Gtk.Adjustment(value=126, lower=1, upper=254, step_increment=10, page_increment=10, page_size=0)
-
-            bri_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=bri)
-            bri_scale.set_value_pos(Gtk.PositionType.RIGHT)
-            bri_scale.set_value(hl_obj.brightness_get())
-            bri_scale.set_digits(0)
-            bri_scale.set_hexpand(True)
-            bri_scale.set_vexpand(False)
-            bri_scale.connect("value-changed", self.bri_scale_moved, hl_obj)
-
-
-            label_tra.set_text("transitiontime")
-
-
-            self.tra_scale.set_value_pos(Gtk.PositionType.RIGHT)
-            self.tra_scale.set_value(hl_obj.transitiontime_get())
-            self.tra_scale.set_digits(0)
-            self.tra_scale.set_hexpand(True)
-            self.tra_scale.set_vexpand(False)
-            self.tra_scale.connect("value-changed", self.tra_scale_moved, hl_obj)
-
-            self.grid.set_border_width(0)
-            self.grid.set_column_homogeneous(True)  # c  r  hs vs
-            #self.grid.set_row_homogeneous(False)  # c  r  hs vs
-            #self.grid.set_column_spacing(40)
-            #self.grid.set_row_spacing(40)
-            self.grid.attach(label_lamp,               0, 0, 1, 1)
-            self.grid.attach(on_off_switch,            0, 1, 1, 1)
-            self.grid.attach(lamp_menu_button,         0, 1, 1, 1)
-            self.grid.attach(label_mir,                1, 0, 1, 1)
-            self.grid.attach(mir_scale,                2, 0, 1, 1)
-            self.grid.attach(label_bri,                1, 1, 1, 1)
-            self.grid.attach(bri_scale,                2, 1, 1, 1)
-            #self.grid.attach(label_tra,                1, 2, 1, 1)
-            #self.grid.attach(tra_scale,                2, 2, 1, 1)
-
-            self.vbox.pack_start(self.grid, False, False, 0)
+            self.vbox.pack_start(dynamic_grid, False, False, 0)
 
         self.add(self.vbox)
+
+    def lamp_dict_mi_selected(self, widget):
+        message_dialog = Gtk.MessageDialog(parent=self, modal=True, destroy_with_parent=True, message_type=Gtk.MessageType.INFO, buttons=Gtk.ButtonsType.CLOSE, text="This shows the lamp_dict.")
+        #message_dialog.set_decorated(False)
+        message_dialog.set_title("lamp_dict")
+        message_dialog.format_secondary_text("The lamp_dict shows the Bulbs recognized \nby the Philips Hue Drive App.\n\n%s" % str(lamp_dict))
+        message_dialog.connect("response", self.dialog_response)
+        message_dialog.show()
+    def dialog_response(self, widget, response_id):
+        if response_id == Gtk.ResponseType.CLOSE:
+            widget.destroy()
 
     def on_off_switch_activated(self, widget, active, hl_obj):
         if widget.get_active():
@@ -202,20 +227,46 @@ class MainWindow(Gtk.ApplicationWindow):
     def quit_menuitem_selected(self, widget):
         app.quit()
 
-    def transitiontime_mi_selected(self, widget, hl_obj, grid, label_tra, tra_scale):
-#        print(widget)
-#        print(hl_obj)
-#        print(grid)
-#        print(label_tra)
-#        print(tra_scale)
+    def mired_rmi_selected(self, widget, hl_obj, dynamic_grid, lamp_label, mir_label, mir_scale, col_label, col_scale):
         if widget.get_active():
-            grid.attach(label_tra,                1, 2, 1, 1)
-            grid.attach(tra_scale,                2, 2, 1, 1)
-            grid.show_all()
+            dynamic_grid.remove_row(0)
+            dynamic_grid.insert_row(0)
+            dynamic_grid.attach(lamp_label,               0, 0, 1, 1)
+            dynamic_grid.attach(mir_label,                1, 0, 1, 1)
+            dynamic_grid.attach(mir_scale,                2, 0, 1, 1)
+            dynamic_grid.show_all()
         else:
-            grid.remove_row(2)
-            #self.reshow_with_initial_size()
-            self.resize(80, 80)
+            dynamic_grid.remove_row(0)
+            dynamic_grid.insert_row(0)
+            dynamic_grid.attach(lamp_label,               0, 0, 1, 1)
+            dynamic_grid.attach(col_label,                1, 0, 1, 1)
+            dynamic_grid.attach(col_scale,                2, 0, 1, 1)
+            dynamic_grid.show_all()
+
+    def brightness_cmi_selected(self, widget, hl_obj, dynamic_grid, bri_label, bri_scale, on_off_switch, lamp_menu_button):
+        if widget.get_active():
+            dynamic_grid.remove_row(1)
+            dynamic_grid.insert_row(1)
+            dynamic_grid.attach(on_off_switch,            0, 1, 1, 1)
+            dynamic_grid.attach(lamp_menu_button,         0, 1, 1, 1)
+            dynamic_grid.attach(bri_label,                1, 1, 1, 1)
+            dynamic_grid.attach(bri_scale,                2, 1, 1, 1)
+            dynamic_grid.show_all()
+        else:
+            dynamic_grid.remove_row(1)
+            dynamic_grid.insert_row(1)
+            dynamic_grid.attach(on_off_switch,            0, 1, 1, 1)
+            dynamic_grid.attach(lamp_menu_button,         0, 1, 1, 1)
+            dynamic_grid.show_all()
+            
+    def transitiontime_cmi_selected(self, widget, hl_obj, dynamic_grid, tra_label, tra_scale):
+        if widget.get_active():
+            dynamic_grid.attach(tra_label,                1, 2, 1, 1)
+            dynamic_grid.attach(tra_scale,                2, 2, 1, 1)
+            dynamic_grid.show_all()
+        else:
+            dynamic_grid.remove_row(2)
+            self.resize(420, 90)
 
 
 
